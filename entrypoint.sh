b1a144bf828fd5d07687abcb6f3c2f822cf52d0f
Sebastian Fiedlschuster <github@fiedlschuster.de>
2019-09-03 03:38:22 +0200

    make script more verbose
    because I don't see why this is failing:
    https://github.com/fiedl/pdf2png-action/commit/d84d566e58f9d3ad5b8c4bc78ea416a52f360f8f/checks

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
 set -e
+set -v
 
-root_dir="$1"
-match="$2"
+export root_dir="$1"
+export match="$2"
+
+echo $root_dir
+echo $match
 
 cd $root_dir
+find . -type f -name "$match" -print0
+
+which convert
+convert -verbose -density 500 -resize 800 "test.pdf" "test-output.png"
+convert -verbose -density 500 -resize 800 "./test.pdf" "test-output-2.png"
+
 find . -type f -name "$match" -print0 |
-  while IFS= read -r -d '' file
-    do convert -verbose -density 500 -resize 800 "${file}" "${file%.*}.png"
+  while IFS= read -r -d '' file; do
+    echo "Converting $file to ${file%.*}.png ..."
+    convert -verbose -density 500 -resize 800 "${file}" "${file%.*}.png"
   done
+
+pwd
+ls
