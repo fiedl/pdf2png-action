64f877ec22d74c3c618c5957f21718b1258a11b4
Sebastian Fiedlschuster <github@fiedlschuster.de>
2019-09-03 04:18:06 +0200

    clean up

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,22 +10,8 @@ echo $root_dir
 echo $match
 
 cd $root_dir
-find . -type f -name "$match" -print0
-
-which convert
-convert -verbose -density 500 -resize 800 "test.pdf" "test-output.png"
-convert -verbose -density 500 -resize 800 "./test.pdf" "test-output-2.png"
-
-find . -type f -name "$match" -print0 |
-  while IFS= read -r -d '' file; do
-    echo "Converting $file to ${file%.*}.png ..."
-    convert -verbose -density 500 -resize 800 "${file}" "${file%.*}.png"
-  done
 
 for pdfile in $match ; do
-  echo "Converting $file to ${file%.*}.png ..."
+  echo "Converting $pdfile to ${pdfile%.*}.png ..."
   convert -verbose -density 500 -resize '800' "${pdfile}" "${pdfile%.*}".png
 done
-
-pwd
-ls
